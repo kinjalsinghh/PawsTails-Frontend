f6b9d514a1554b0578f04a5e13f163ac9c7d5eae
kinjalsinghh <118274996+kinjalsinghh@users.noreply.github.com>
2024-08-10 18:07:22 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@ import ShopContextProvider from './Context/ShopContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <ShopContextProvider>
     <App />
     </ShopContextProvider>
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
